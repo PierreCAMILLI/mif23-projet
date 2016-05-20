a3d54923892e20ba2eb97fd24dbed2d5ab0a2d4a
PierreCAMILLI <camillipierre@gmail.com>
2016-05-20 11:36:18 +0200

    Bon affichage des cercles.

--- tutos/tp2.cpp
+++ tutos/tp2.cpp
@@ -45,84 +45,52 @@ Ray make_ray( const Point& o, const Point& e )
     return r;
 }
 
-class Sphere : public Objecta
+struct Sphere
 {
-    public:
-        Point c;
-        float rayon;
-
-        Sphere(Point p, float r, Color co) : c(p), rayon(r)
-        {couleur = co;};
-        Sphere(const Sphere & sph) : c(sph.c), rayon(sph.rayon)
-        {couleur = sph.couleur;};
-        const Sphere & operator=(const Sphere & sph)
-        {
-        c = sph.c; rayon = sph.rayon;
-        couleur = sph.couleur;
-        return *this;};
-        bool intersect(Ray ray,Hit &hit)
-        {
-            std::cout << "Sphere::intersect()" << std::endl;
-            float a = dot(ray.direction, ray.direction);
-            float b = 2*dot(ray.direction, make_vector(c, ray.origin));
-            float c = dot(make_vector(this->c, ray.origin),make_vector(this->c, ray.origin)) - rayon*rayon;
-            float delta = b*b - 4*a*c;
-            if(delta <= 0)
-                return false;
-            else
-            {
-                float t1 = (-b + sqrtf(delta))/(2*a);
-                float t2 = (-b - sqrtf(delta))/(2*a);
-                float ttemp;
-                ttemp = (t1 < t2) ? t1 : t2;
-                if(hit.t > ttemp){
-                    hit.t = ttemp;
-                    hit.n = ray.direction;
-                    hit.p = ray.origin;
-                    return true;
-                }
-                return false;
-            }
-        }
+    Transform model;
+    Point c;
+    float rayon;
+    Color couleur;
 };
 
-class Plan : public Objecta
+Sphere make_sphere( const Transform& model, const Point& center, const float radius )
 {
-    public:
-        Point a;
-        Vector normal;
+    Sphere s;
+    s.model= model;
+    s.c= center;
+    s.rayon= radius;
+    s.couleur = make_color(0,0,0);
+    return s;
+}
 
-        Plan(Point p, Vector n, Color c) : a(p), normal(n)
-        {couleur = c;};
-        Plan(const Plan & pla) : a(pla.a), normal(pla.normal)
-        {couleur = pla.couleur;};
-        const Plan & operator=(const Plan & pla)
-        {
-        a = pla.a; normal = pla.normal;
-        couleur = pla.couleur;
-        return *this;};
-        bool intersect(Ray ray,Hit &hit)
-        {
-            std::cout << "Plan::intersect()" << std::endl;
-            float t;
-            t = dot(make_vector(a, ray.origin),normal)/ dot(ray.direction, normal);
+Sphere make_sphere( const Transform& model, const Point& center, const float radius, const Color c )
+{
+    Sphere s = make_sphere(model, center, radius);
+    s.couleur.r = c.r;
+    s.couleur.g = c.g;
+    s.couleur.b = c.b;
+    s.couleur.a = c.a;
+    return s;
+}
 
-            if(t < 0)
-                return false;
-            else
-            {
-                if(hit.t > t){
-                    hit.t = t;
-                    hit.n = ray.direction;
-                    hit.p = ray.origin;
-                    return true;
-                }
-                else return false;
-            }
-        }
+struct Plan
+{
+    Point a;
+    Vector normal;
+    Color couleur;
 };
 
+std::vector<Sphere> spheres;
 
+std::vector<Plan> plans;
+
+void add(Plan plan){
+    plans.push_back(plan);
+}
+
+void add(Sphere sphere){
+    spheres.push_back(sphere);
+}
 
 bool intersect(Plan plan,Ray ray,Hit &hit)
 {
@@ -167,6 +135,18 @@ bool intersect(Sphere sphere,Ray ray,Hit &hit)
     }
 }
 
+bool intersect(Ray ray,Hit &hit){
+    for(std::vector<Sphere>::iterator it = spheres.begin(); it != spheres.end(); it++){
+        if(intersect((*it), ray, hit))
+            return true;
+    }
+    for(std::vector<Plan>::iterator it = plans.begin(); it != plans.end(); it++){
+        if(intersect((*it), ray, hit))
+            return true;
+    }
+    return false;
+}
+
 void DrawPlane(Plan p)
 {
 
@@ -176,18 +156,26 @@ void DrawSphere(Sphere s)
 
 }
 
-Color getCouleurIntersect(std::vector<Objecta> objs, Ray r){
+Color getCouleurIntersect(Ray r, Hit &hit){
     float min_hit = FLT_MAX;
-    Color intersect_col = objs[0].couleur;
-    Hit hit = {{0,0,0}, make_vector({0,0,0}, {0,0,0}), 0};
+    Color intersect_col = plans[0].couleur;
+    hit = {{0,0,0}, make_vector({0,0,0}, {0,0,0}), 0};
 
-    for(std::vector<Objecta>::iterator it = objs.begin(); it != objs.end(); it++){
-        (*it).intersect(r, hit);
+    for(std::vector<Sphere>::iterator it = spheres.begin(); it != spheres.end(); it++){
+        if(intersect((*it), r, hit))
         if(hit.t < min_hit){
             min_hit = hit.t;
             intersect_col = (*it).couleur;
         }
     }
+    for(std::vector<Plan>::iterator it = plans.begin(); it != plans.end(); it++){
+        if(intersect((*it), r, hit))
+        if(hit.t < min_hit){
+            min_hit = hit.t;
+            intersect_col = (*it).couleur;
+        }
+    }
+    hit.t = min_hit;
     return intersect_col;
 }
 
@@ -206,15 +194,15 @@ int main( int agc, char **argv )
     Point b = {0.5f,0.5f,0.5f};
     Point c = {-1.0f,-1.0f,-1.0f};
     Vector n = {0.5f, 1.0f, 0.0f};
-    Plan plan(a, n, make_color(255,255,255));
-    Sphere sphere1(a, 0.5, make_color(255,0,0));
-    Sphere sphere2(b, 0.5, make_color(0,255,0));
-    Sphere sphere3(c, 0.5, make_color(0,0,255));
+    Plan plan{a, n, make_color(255,255,255)};
+    Sphere sphere1{make_identity(), a, 0.5, make_color(255,0,255)};
+    Sphere sphere2{make_identity(), b, 0.5, make_color(0,255,0)};
+    Sphere sphere3{make_identity(), c, 0.5, make_color(0,0,255)};
     std::vector<Objecta> objs;
-    objs.push_back(plan);
-    objs.push_back(sphere1);
-    objs.push_back(sphere2);
-    objs.push_back(sphere3);
+    add(plan);
+    add(sphere1);
+    add(sphere2);
+    add(sphere3);
 
     for(int y= 0; y < image.height; y++)
     for(int x= 0; x < image.width; x++)
@@ -224,38 +212,8 @@ int main( int agc, char **argv )
         Point e = {0.0f, 0.0f, 5.0f};
         Ray ray = make_ray(o, e);
 
-        // calculer les intersections
-        /*
-        Hit hit = {o, make_vector(o, e), 0};
-        intersect(plan, ray, hit);
-        float tplan = hit.t;
-
-        intersect(sphere1, ray, hit);
-        float tsphere1 = hit.t;
-
-        intersect(sphere2, ray, hit);
-        float tsphere2 = hit.t;
-        */
-
-        image_set_pixel(image, x, y, getCouleurIntersect(objs, ray));
-        /*
-        if(tplan < 0 && tsphere1 < 0 ){
-            image_set_pixel(image, x, y, make_color(0, 0, 0));
-        }
-        if(tsphere1 > tsphere2){
-
-        }
-        if(tplan > tsphere1)
-        {
-            // calculer une couleur pour le pixel
-            //Point p = hit.p + hit.t*hit.n;
-            image_set_pixel(image, x, y, sphere1.couleur);
-        }
-        else
-        {
-            image_set_pixel(image, x, y, plan.couleur);
-        }
-        */
+        Hit hit;
+        image_set_pixel(image, x, y, getCouleurIntersect(ray, hit));
     }
 
     // enregistrer l'image
